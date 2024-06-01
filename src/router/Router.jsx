4f85f67dcc57efbfa246072d13641278795ef62e
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-01 12:07:01 -0500

    add error element to router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import Home from '../home/pages/Home';
+import { ErrorPage } from '../error/pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+    ],
   },
 ]);
 
