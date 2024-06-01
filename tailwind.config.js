1a3955542008be59911fed575ae786f087511c29
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-01 12:05:32 -0500

    add new colours green

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#f9f7f3',
+        secondary: '#1b2629',
+        btnColor: '#9c702a',
+        veggie: '#1aa705',
+        'veggie-light': '#47b836',
+      },
+    },
   },
   plugins: [],
 };
