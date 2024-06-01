de628b3dc75ee42898610c70a554d49a21cb1271
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-01 16:28:53 -0500

    add category array

---
+++ src/category/constants/index.js
@@ -0,0 +1,38 @@
+export const categories = [
+  {
+    name: 'entrees',
+    href: 'entrees',
+    backgroundColor: '#f0f5c4',
+    color: '#59871f',
+  },
+  {
+    name: 'breakfast',
+    href: 'breakfast',
+    backgroundColor: '#efedfa',
+    color: '#3c3a8f',
+  },
+  {
+    name: 'lunch',
+    href: 'lunch',
+    backgroundColor: '#e5f7f3',
+    color: '#1f8787',
+  },
+  {
+    name: 'desserts',
+    href: 'desserts',
+    backgroundColor: '#e8f5fa',
+    color: '#397a9e',
+  },
+  {
+    name: 'sides',
+    href: 'sides',
+    backgroundColor: '#feefc9',
+    color: '#d16400',
+  },
+  {
+    name: 'drinks',
+    href: 'drinks',
+    backgroundColor: '#ffeae3',
+    color: '#f0493e',
+  },
+];
