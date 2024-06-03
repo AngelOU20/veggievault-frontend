ca938b45e0af1cfb0fe8b7d10037a74ba623f27c
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-03 15:59:41 -0500

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import './styles/App.css';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="max-w-screen-2xl mx-auto">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
