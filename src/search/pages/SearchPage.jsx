ec77a5429165e5f016444b89c031ea03cad301f2
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-04 13:29:48 -0500

    add Spinner to search page

--- src/search/pages/SearchPage.jsx
+++ src/search/pages/SearchPage.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { GrSearch } from 'react-icons/gr';
-import { Card } from '../../components/Card/Card';
+import { Card, Spinner } from '../../components';
 
 const SearchPage = () => {
   const [query, setQuery] = useState('');
@@ -44,7 +44,7 @@ const SearchPage = () => {
   return (
     <div className="px-6 lg:px-12 py-20">
       <h1 className="text-center text-3xl py-10 font-semibold text-secondary sm:text-6xl sm:leading-relaxed">
-        Search
+        Search for a recipe
       </h1>
 
       <div className="bg-white md:max-w-3xl mx-auto p-4 rounded realtive flex items-center">
@@ -61,12 +61,13 @@ const SearchPage = () => {
         />
       </div>
 
-      {isLoading && <div>Loading...</div>}
+      {isLoading && <Spinner />}
 
       {error && <div>{error}</div>}
 
       <ul className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {results &&
+        {!isLoading &&
+          results &&
           results?.map((result) => (
             <React.Fragment key={result.name}>
               <Card item={result} />
