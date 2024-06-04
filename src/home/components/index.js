68d4b4bfa3978d56bbb2e6dec8112a021b841aa9
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-04 13:26:43 -0500

    add barrel file to home page

---
+++ src/home/components/index.js
@@ -0,0 +1,6 @@
+export * from './AboutSection';
+export * from './CompanyLogo';
+export * from './FeaturedSection';
+export * from './Hero';
+export * from './LatestRecipe';
+export * from './NewsLetter';
