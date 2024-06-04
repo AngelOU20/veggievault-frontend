e252224c3e18817f9165027d1bc3e079440bc9db
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-04 13:31:11 -0500

    add pagination and spinner component

--- src/recipes/pages/Recipes.jsx
+++ src/recipes/pages/Recipes.jsx
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Card } from '../../components/Card/Card';
+import { Card, Spinner } from '../../components';
+import { useSearchParams } from 'react-router-dom';
 
 const Recipes = () => {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [totalPages, setTotalPages] = useState(0);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = searchParams.get('page') || 1;
 
   useEffect(() => {
     const fetchRecipes = async () => {
@@ -13,9 +17,10 @@ const Recipes = () => {
 
       try {
         const response = await axios.get(
-          'http://localhost:3000/api/all-recipes'
+          `http://localhost:3000/api/paginated-recipes?page=${page}&limit=8`
         );
-        setResults(response.data);
+        setResults(response.data.recipes);
+        setTotalPages(response.data.totalPages);
         setIsLoading(false);
         setError(null);
       } catch (error) {
@@ -26,7 +31,11 @@ const Recipes = () => {
     };
 
     fetchRecipes();
-  }, []);
+  }, [page]);
+
+  const handlePageChange = (newPage) => {
+    setSearchParams({ page: newPage });
+  };
 
   return (
     <div className="px-6 lg:px-12 py-20">
@@ -34,17 +43,32 @@ const Recipes = () => {
         All Recipes
       </h1>
 
-      {isLoading && <div>Loading...</div>}
+      {isLoading && <Spinner />}
 
       {error && <div>{error}</div>}
 
       <ul className="mt-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-        {results?.map((item) => (
-          <React.Fragment key={item._id}>
-            <Card item={item} />
-          </React.Fragment>
-        ))}
+        {!isLoading &&
+          results?.map((item) => (
+            <React.Fragment key={item._id}>
+              <Card item={item} />
+            </React.Fragment>
+          ))}
       </ul>
+
+      <div className="flex justify-center mt-10">
+        {Array.from({ length: totalPages }, (_, i) => (
+          <button
+            key={i}
+            onClick={() => handlePageChange(i + 1)}
+            className={`px-4 py-2 mx-1 ${
+              page === i + 1 ? 'bg-secondary' : 'bg-gray-200'
+            } hover:bg-secondary hover:text-white`}
+          >
+            {i + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
