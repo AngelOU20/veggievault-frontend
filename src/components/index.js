0549ccf3438972e9441b43fc98c051906f4d16a8
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-04 13:15:59 -0500

    add exports of Skeleton and Spinner

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,5 @@ export * from './Card/Card';
 export * from './Navbar/DesktopNav';
 export * from './Navbar/MobileNav';
 export * from './Header';
+export * from './Skeleton';
+export * from './Spinner';
