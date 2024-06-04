79a8ffd65758d144a57e9df10d5891cf0341079a
Julio Ucharima <julio_ucharima@usmp.pe>
2024-06-04 13:13:29 -0500

    add Spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+export const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="spinner-circle"></div>
+      <div className="spinner-circle"></div>
+      <div className="spinner-circle"></div>
+    </div>
+  );
+};
